57a04fef166554648cccd399ea846635407100b8
Shaheer-rossoneri14 <hshaheer99@gmail.com>
2021-06-06 19:44:20 +0530

    Second  commit

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Book, BookInstance, Genre
+
+#admin.site.register(Book)
+#admin.site.register(Author)
+#admin.site.register(BookInstance)
+admin.site.register(Genre)
+
+#Define Admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'country')
+
+#Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+   list_display = ('title', 'author', 'display_genre')
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
